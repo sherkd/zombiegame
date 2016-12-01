cc1c9ad837ad7f986987f648dc0b7ba585d495f0
Morris <morriskeehnen@gmail.com>
2016-12-01 13:11:47 +0100

    Added

---
+++ WebGame/WebGame/game/database/class1.py
@@ -0,0 +1,13 @@
+﻿class Database(object):
+    """description of class"""
+    def __init__(self):
+        self.db = pymysql.connect(host="104.219.248.113 ",    # your host, usually localhost
+                         user="amatfbgg_infdev6",         # your username
+                         passwd="DbX3xb6K#nyE",  # your password
+                         db="amatfbgg_infdev06b")        # name of the data base
+    def getDB(self):
+        return self.db
+
+    def connect(self):
+        cur = db.cursor()
+        return cur.execute("Create Table Users (id int(25))")
