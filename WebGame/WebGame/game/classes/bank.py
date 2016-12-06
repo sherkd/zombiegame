db82c1028c5e929ea25b15a0f385bb8cf2e7a5ac
Shervin <0897669@hr.nl>
2016-12-06 16:40:35 +0100

    bank and search fixed met sivar

--- WebGame/WebGame/game/classes/bank.py
+++ WebGame/WebGame/game/classes/bank.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-import player
+from game.classes.player import Player
 
 class Bank(object):
     """description of bank."""
@@ -33,83 +33,3 @@ class TestBank(TestCase):
         self.assertEquals(0, bank.player.getBalance())
         bank.depositMoney(10)
         self.assertEquals(40, bank.player.getMoney())
-
-class Player(object):
-
-    def __init__(self, username, health, attack, cooldowns, money, items, level, experience, balance):
-        self.username = username
-        self.health = health
-        self.attack = attack
-        self.cooldowns = cooldowns
-        self.money = money
-        self.items = items
-        self.level = level
-        self.experience = experience
-        self.balance = balance
-
-    def getUserName(self):
-        return self.username
-
-    def getHealth(self):
-        return self.health
-
-    def setHealth(self, health):
-        self.health = health
-
-    def getAttackDamage(self):
-        return self.attack
-
-    def setAttackDamage(self, damage):
-        self.attack = damage
-       
-    def getCooldowns(self):
-        return self.cooldowns
-
-    def removeCooldown(self, cooldown):
-        self.cooldowns.remove(cooldown)
-
-    def addCooldown(self, cooldown):
-        self.cooldowns.append(cooldown)
-
-    def getMoney(self):
-        return self.money
-
-    def setMoney(self, money):
-        self.money = money
-
-    def getBalance(self):
-        return self.balance
-
-    def setBalance(self, balance):
-        self.balance = balance
-
-    def getItems(self):
-        return self.items
-
-    def removeItem(self, item):
-        self.items.remove(item)
-
-    def addItem(self, item):
-        self.items.append(item)     
-
-    def getLevel(self):
-        return self.level;
-
-    def increaseLevel(self):
-        self.level += 1
-
-    def getExperience(self):
-        return self.experience
-
-    def setExperience(self, exp):
-        self.experience = exp
-
-    def getRequiredExp(self):
-        return self.level * 140
-
-    def checkLevelUp(self):
-        if self.experience >= self.getRequiredExp():
-            self.increaseLevel()
-            self.setExperience(self.getRequiredExp() - self.experience)
-            return True
-        return False
--- WebGame/WebGame/game/levels/searchlevel.py
+++ WebGame/WebGame/game/levels/searchlevel.py
@@ -1,6 +1,7 @@
 import random
 from django.test import TestCase
 import time, threading
+from game.classes.player import Player
 
 class SearchLevel(object):
     
@@ -49,7 +50,7 @@ class SearchLevel(object):
 class TestSearch(TestCase):
 
     def __init__(self, methodName = 'runTest'):
-        self.search = SearchLevel(Player("name", 100, 10, ["cl"], 50, ["items"], 1, 20), 10)
+        self.search = SearchLevel(Player("name", 100, 10, ["cl"], 50, ["items"], 1, 20, 0), 10)
         return super().__init__(methodName)
     
     def testSearch(self):
@@ -57,84 +58,3 @@ class TestSearch(TestCase):
         self.search.startScouting()
         self.assertEquals(["cl", "scouting"], self.search.getPlayer().getCooldowns())
         self.assertEquals(9, self.search.getSeconds())
-
-#Remove after testing, can't seem to fix the import.
-class Player(object):
-
-    def __init__(self, username, health, attack, cooldowns, money, items, level, experience):
-        self.username = username
-        self.health = health
-        self.attack = attack
-        self.cooldowns = cooldowns
-        self.money = money
-        self.items = items
-        self.level = level
-        self.experience = experience
-
-    def getUserName(self):
-        return self.username
-
-    def getHealth(self):
-        return self.health
-
-    def setHealth(self, health):
-        self.health = health
-
-    def getAttackDamage(self):
-        return self.attack
-
-    def setAttackDamage(self, damage):
-        self.attack = damage
-       
-    def getCooldowns(self):
-        return self.cooldowns
-
-    def removeCooldown(self, cooldown):
-        self.cooldowns.remove(cooldown)
-
-    def addCooldown(self, cooldown):
-        self.cooldowns.append(cooldown)
-
-    def getMoney(self):
-        return self.money
-
-    def setMoney(self, money):
-        self.money = money
-
-    def getItems(self):
-        return self.items
-
-    def removeItem(self, item):
-        self.items.remove(item)
-
-    def addItem(self, item):
-        self.items.append(item)     
-
-    def getLevel(self):
-        return self.level;
-
-    def increaseLevel(self):
-        self.level += 1
-
-    def getExperience(self):
-        return self.experience
-
-    def setExperience(self, exp):
-        self.experience = exp
-
-    def getRequiredExp(self):
-        return self.level * 140
-
-    def checkLevelUp(self):
-        if self.experience >= self.getRequiredExp():
-            self.increaseLevel()
-            self.setExperience(self.getRequiredExp() - self.experience)
-            return True
-        return False
-
-
-
-
-
-
-
