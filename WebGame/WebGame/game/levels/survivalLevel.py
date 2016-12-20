29a27091db31ff5458d395bc07996f9d52772c59
Shervin <0897669@hr.nl>
2016-12-20 14:12:41 +0100

    survival logic fixed

--- WebGame/WebGame/game/levels/survivalLevel.py
+++ WebGame/WebGame/game/levels/survivalLevel.py
@@ -5,9 +5,10 @@ from game.classes.enemy import Enemy
 class Survival(object):
     """description of bank."""
 
-    def __init__(self, player, wavecounter):
+    def __init__(self, player, wavecounter, damage):
         self.player = player
         self.wavecounter = wavecounter
+        self.damage = damage
 
     def getPlayer(self):
         return self.player
@@ -15,16 +16,33 @@ class Survival(object):
     def getWaveCounter(self):
         return self.wavecounter
 
+    def setWaveCounter(self, wavecounter):
+        self.wavecounter = wavecounter
+
+    def getDamage(self):
+        return self.damage
+
+    def setDamage(self, damage):
+        self.damage = damage
+
     def getEnemy(self):
         return self.enemy
 
+    def oneWave(self):
+        if self.player.health > 0 and self.player.health >= self.damage:
+            self.player.setHealth(self.player.getHealth() - self.damage)
+            self.setWaveCounter(self.getWaveCounter() + 1)
+            self.setDamage(self.getDamage() + 10)                 
+        else:
+            pass
+
     def startWaves(self):
-        self.wavecounter = 1
         damage = 10
         while startWaves():
-            if self.player.getHealth > 0 and self.player.getHealth - damage != 0:
-                self.wavecounter = self.wavecounter + 1
-                damage = damage + 10
+            if self.player.health > 0 and self.player.health > damage:
+                self.player.setHealth(self.player.getHealth() - damage)
+                self.setWaveCounter(self.getWaveCounter() + 1)
+                damage = damage + 10     
             else:
                 break
 
@@ -33,15 +51,44 @@ class Survival(object):
         while len(self.player.getCooldowns()) == 1:
             if self.player.health <= 0:
                 self.player.removeCooldown("Survival")
-            else:
-                startWaves()
-                break
+            elif self.player.health > 0 and self.player.health > damage:
+                self.player.setHealth(self.player.getHealth() - damage)
+                self.setWaveCounter(self.getWaveCounter() + 1)
+                damage = damage + 10
+                #startWaves()
+                
 
 
 class TestSurvival(TestCase):
 
     def testSurvival(self):
         #Player(username, health, attack, cooldowns, money, items, weapons, level, experience, balance)
-        surv = Survival(Player("name", 100, 10, ["cl"], 50, ["items"], 1, 20, 0, 0),1)
+        surv = Survival(Player("name", 100, 10, ["cl"], 50, ["items"], 1, 20, 0, 0), 1, 10)
+
         #Enemy(name, level, health, weapon, rewardItem)
         #en = Enemy("Henk", 3, 100, Weapon("Henk weapon", 5, 100), None)
+
+        self.assertEquals(100, surv.player.getHealth())
+
+        self.assertEquals(1, surv.getWaveCounter())
+
+        surv.oneWave()
+
+        self.assertEquals(2, surv.getWaveCounter())
+        self.assertEquals(90, surv.player.getHealth())
+
+        surv.oneWave()
+
+        self.assertEquals(3, surv.getWaveCounter())
+        self.assertEquals(70, surv.player.getHealth())
+
+        surv.oneWave()
+
+        self.assertEquals(4, surv.getWaveCounter())
+        self.assertEquals(40, surv.player.getHealth())
+
+        surv.oneWave()
+
+        self.assertEquals(5, surv.getWaveCounter())
+        self.assertEquals(0, surv.player.getHealth())
+
